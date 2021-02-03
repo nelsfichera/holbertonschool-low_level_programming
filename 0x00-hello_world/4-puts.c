0bd950cdcae468c935ae4ed39b8d6300373d2007
Nels <nelsfichera@gmail.com>
2021-02-03 18:47:37 -0500

    4puts with betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+/**
+ * main - puts a line of text on stdout
+ * Return: 0
+ */
+{
+	puts '"Programming is like a building a multilingual puzzle"';
+
+	return (0);
+}
