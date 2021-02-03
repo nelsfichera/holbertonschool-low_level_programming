bdef678d15cc5fc3eca73d65e4aa4810df98d336
Nels <nelsfichera@gmail.com>
2021-02-03 18:56:16 -0500

    Betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-int main(void)
 /**
- * main - puts a line of text on stdout
- * Return: 0
- */
+* main - puts a line of text on stdout
+* Return: 0
+*/
+int main(void)
 {
-	puts '"Programming is like a building a multilingual puzzle"';
+	puts("\"Programming is like a building a multilingual puzzle");
 
 	return (0);
 }
