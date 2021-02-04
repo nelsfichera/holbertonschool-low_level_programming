231f7863c288198de889e8d9bbb4973a94b30b0e
Nels <nelsfichera@gmail.com>
2021-02-03 19:01:04 -0500

    \n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-	puts("\"Programming is like a building a multilingual puzzle");
+	puts("\"Programming is like a building a multilingual puzzle\n");
 
-	return(0);
+	return (0);
 }
