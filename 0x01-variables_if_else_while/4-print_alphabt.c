4f8e7a184a1f54398c14ef87686b7efe13e0ef7b
Nels <nelsfichera@gmail.com>
2021-02-04 16:04:48 -0500

    Or operand in while loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - print alpha no q no e
+* Return: 0
+*/
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x != 'e' || x != 'q')
+		{
+			putchar(x);
+			x++;
+	}
+	}
+	putchar('\n');
+	return (0);
+}
