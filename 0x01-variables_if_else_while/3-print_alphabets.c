5cf25f4a4e3c5fbc94b6e2ab7e61398072e1bcaf
Nels <nelsfichera@gmail.com>
2021-02-04 15:27:06 -0500

    Two while loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - print alpha ALPHA
+* Return: 0
+*/
+
+int main(void)
+{
+	int x = 'a';
+	int y = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
