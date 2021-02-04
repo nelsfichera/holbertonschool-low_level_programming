031a51fe5ed1f6f878eb1296aaf462f3c75ea8c8
Nels <nelsfichera@gmail.com>
2021-02-04 14:47:15 -0500

    Remove ctype

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
 * main - print alphabet w/new line but don't use printf bc we don't like fun
 * Return: 0
@@ -8,7 +7,7 @@ int main(void)
 {
 	for (int x = 'a'; x <= 'z'; x++)
 	{
-		putchar;
+		putchar(x);
 	}
 		putchar('\n');
 		return (0);
