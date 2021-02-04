2ff7d7635331141adf857bf6e485ba3def04d9c5
Nels <nelsfichera@gmail.com>
2021-02-04 17:19:31 -0500

    Putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+/**
+* main - 0-9 but putchar
+* Return: 0
+*/
+int main(void)
+{
+	int x = 0;
+
+	while (x <=9)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0); 
+}
