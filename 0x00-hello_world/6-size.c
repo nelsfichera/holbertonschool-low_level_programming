ff8444ab06b9443246b625fc46e8d1e1559ccb28
Nels <nelsfichera@gmail.com>
2021-02-03 20:02:50 -0500

    Betty compliant 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - printfs nested in sizeof to tell us the size of things
+* Return: a list of the sizes of the types, naturally
+*/
+int main(void)
+{
+	printf("Size of a char: %2d byte\(s\) \n", sizeof(char));
+	printf("Size of an int: %2d byte\(s\) \n", sizeof(int));
+	printf("Size of a long int: %2d byte\(s\) \n", sizeof(long int));
+	printf("Size of a long long int: %2d byte\(s\) \n", sizeof(long long int));
+	printf("Size of a float: %2d byte\(s\) \n", sizeof(float));
+
+	return (0);
+}
