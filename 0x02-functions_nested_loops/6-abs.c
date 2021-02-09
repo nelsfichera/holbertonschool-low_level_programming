39d3696d8b0b8b34e300a7b4b5f17d9b78bb1f27
Nels <nelsfichera@gmail.com>
2021-02-09 14:17:52 -0500

    Simplify structure

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,3 +1,14 @@
-int _abs(int){
-	int *(-1);
+#include "holberton.h"
+/**
+* _abs - absolute value
+* @x: the number
+* Return: absval
+*/
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	return (x);
 }
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int abs(int);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
