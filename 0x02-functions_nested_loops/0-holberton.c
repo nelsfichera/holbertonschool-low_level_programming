1d221fd557e3d06341dfbe45f6032f2d2d0b8594
Nels <nelsfichera@gmail.com>
2021-02-09 09:45:52 -0500

    Compiled

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,9 +9,9 @@ int main(void)
 	int i;
 	char *text = "Holberton\n";
 
-	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(*(text+i));
+		_putchar(*(text + i));
 	}
 	return (0);
 }
