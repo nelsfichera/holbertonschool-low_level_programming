b7523d1f16f4ac27ee16dc496eaa6758ed14cd08
Nels <nelsfichera@gmail.com>
2021-02-09 13:40:00 -0500

    Is it an alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,15 @@
+#include "holberton.h"
+/**
+* _isalpha - tell the stupid computer if it's a letter
+* @c: the variable
+* Return: 1 if true, 0 if false
+*/
 int _isalpha(int c)
 {
-	if (c == a - z && c == A - Z)
+	if (c <= 'z')
 		return (1);
-	else 
+	else if (c <= 'Z')
+		return (1);
+	else
 		return (0);
 }
