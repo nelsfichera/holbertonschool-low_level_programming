5c6f6ea12b7f873cd1ebc7e27706b9bc72d122c1
Nels <nelsfichera@gmail.com>
2021-02-09 13:18:08 -0500

    Commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,14 @@
+#include "holberton.h"
+/**
+* _islower - is it lower or not
+* Return: 1 if yes 0 if no
+*/
+
 int _islower(int c)
 {
+
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
-		return (0); 
+	else
+		return (0);
 }
