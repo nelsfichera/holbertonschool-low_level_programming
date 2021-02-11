1df4867eefbb3b1e3187648bed5f42998d31497d
Nels <nelsfichera@gmail.com>
2021-02-11 14:31:47 -0500

    Putchar only does one character, silly

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + j);
+			if (x > 9)
+			{
+			_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
