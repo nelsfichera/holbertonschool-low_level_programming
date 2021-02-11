6932d5948a61bc0bd1c70aa9d7cbb99a5cabbb3f
Nels <nelsfichera@gmail.com>
2021-02-11 13:00:53 -0500

    Fix variables, formatting

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
  #include "holberton.h"
-  2 /**
-  3 * _isdigit - is it number or not
-  4 * @c: c is a thing that i'm doing
-  5 * Return: 1 if yes 0 if no
-  6 */
-  7 
-  8 int _isdigit(int c)
-  9 {
- 10 
- 11         if (c >= '0' && c <= '9')
- 12                 return (1);
- 13         else
- 14                 return (0);
- 15 }
+ /**
+ * _isdigit - is it number or not
+ * @c: c is a thing that i'm doing
+ * Return: 1 if yes 0 if no
+ */
+
+int _isdigit(char digit)
+{
+
+	if (digit >= '0' && digit <= '9')
+		return (1);
+	else
+		return (0);
+}
