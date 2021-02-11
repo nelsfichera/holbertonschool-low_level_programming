04ffd3a26edd8545aabe211047268622bd27f7dd
Nels <nelsfichera@gmail.com>
2021-02-11 13:45:06 -0500

    Simple x2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_numbers - numnumbers
+* Return: v0!d
+*/
+void print_numbers(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
+
