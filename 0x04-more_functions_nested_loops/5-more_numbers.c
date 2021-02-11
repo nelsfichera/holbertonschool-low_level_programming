a84e2b65e4d407c575d4ace3e14bf9341ed78536
Nels <nelsfichera@gmail.com>
2021-02-11 14:27:11 -0500

    Nested for with if

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* more_numbers - 0 to 14 x10
+* Return: void
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+}
