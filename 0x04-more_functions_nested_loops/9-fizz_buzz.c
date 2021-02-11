2a84888e818150ff2b9d4b6ab5ae7fd5a7ba4fc5
Nels <nelsfichera@gmail.com>
2021-02-11 17:14:28 -0500

    Fix spacing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; i < 100; x++)
+	for (x = 1; x < 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 		{
@@ -23,7 +23,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", x);
+			printf("%d ", x);
 		}
 	}
 	printf("Buzz\n");
