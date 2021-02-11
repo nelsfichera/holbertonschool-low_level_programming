a7ece13711d41777b2479b5847557a71a3785337
Nels <nelsfichera@gmail.com>
2021-02-11 17:11:49 -0500

    One function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 /**
-* fizz_buzz - fizzbuzz
+* main - fizzbuzz
 * Return: 0
 */
-void fizz_buzz(void)
-{
-	int i;
-
-	for (i = 0; i <= 100; i++)
-	{
-		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d", i);
-		printf(" ");
-	}
-	putchar('\n');
-}
-
-/**
-* main - calls fizz_buzz
-* Return: 0
-*/
-
 int main(void)
 {
-	fizz_buzz();
+	int x;
+
+	for (x = 1; i < 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+	}
+	printf("Buzz\n");
 	return (0);
 }
