7a558a337474f7bb073e8fb8666812de97f0ef84
Nels <nelsfichera@gmail.com>
2021-02-15 14:18:34 -0500

    Init

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+int main(void)
+{
+	int x; 
+	int y; 
+
+	for(x='a'; x<='z';x++)
+	{
+		_putchar(x);
+	}
+	for(y='A'; x<='Z'; y++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');
+}
