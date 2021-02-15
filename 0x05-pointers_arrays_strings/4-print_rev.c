19321b66f1486b1abe5eeb93577107cca50d8798
Nels <nelsfichera@gmail.com>
2021-02-15 16:04:00 -0500

    mains

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* print_rev - twin peaks it
+* @s: the string 
+* Return: v0iddddddd
+*/
+void print_rev(char *s);
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* name_of_function - description here
+* @s: the str
+* Return: void
+*/
+void rev_string(char *s)
+{
+
+
