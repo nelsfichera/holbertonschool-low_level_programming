7a802180c83ae129826cd59ece29dcc43d8c69cd
Nels <nelsfichera@gmail.com>
2021-02-16 13:17:07 -0500

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,18 @@
 */
 void rev_string(char *s)
 {
+	int x;
+	int len;
+	int temp = 0;
 
-
-
+	for (len = 0; s[len] != 0; len++)
+	{
+		continue;
+	}
+	for (x = 0; x < len / 2; x++)
+	{
+		temp = s[x];
+		s[x] = s[len - 1 - x];
+		s[len - 1 - x] = temp;
+	}
+}
