a6120ef8b63bc9eddf70f0b87e839719fb3aa203
Nels <nelsfichera@gmail.com>
2021-02-19 10:43:58 -0500

    Compare 2 strings no vars

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _strcmp - compare the strings
+* @s1: str 1
+* @s2: str 2
+* Return: diff
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
