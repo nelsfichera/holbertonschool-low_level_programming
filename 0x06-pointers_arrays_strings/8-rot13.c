44107cb7b33304020d7b144225611217eb89c775
Nels <nelsfichera@gmail.com>
2021-02-19 13:50:59 -0500

    Rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,3 +6,21 @@
 */
 char *rot13(char *r)
 {
+	int x, y;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; r[x] != '\0'; x++)
+	{
+		for (y = 0; alpha[y] != '\0'; y++)
+		{
+			if (r[x] == alpha[y])
+			{
+				r[x] = rot[y];
+				break;
+			}
+		}
+	}
+	return (r);
+}
+
