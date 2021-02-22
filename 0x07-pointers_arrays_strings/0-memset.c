d95d7444cd8be158597c9b04500a1d8d41e30b3e
Nels <nelsfichera@gmail.com>
2021-02-22 09:43:18 -0500

    memset init

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,3 +8,14 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
+	if (s != (void *)0 && n > 0)
+	{
+		unsigned int i = 0;
+
+		for (i = 0; i < n; i++)
+		{
+			*s++ = b;
+		}
+	}
+	return (s);
+}
