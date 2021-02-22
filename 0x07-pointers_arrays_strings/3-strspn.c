7d975379b5bbfaa2c2c5ce67d88a03c008e361b3
Nels <nelsfichera@gmail.com>
2021-02-22 11:16:59 -0500

    strspn init

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,4 +7,18 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int i; /*for iteration*/
+	unsigned int j; /*for iteration*/
 
+	for (i = 0; accept[x] != '0' i++)
+	{
+		for (j = 0; s[j] != '0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				return (j);
+			}
+		}
+	}
+	return (NULL);
+}
