b077bf7b8826042c3c8d81083ecc69f163a7091d
Nels <nelsfichera@gmail.com>
2021-02-22 09:53:11 -0500

    memcpy init

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,3 +8,10 @@ include "holberton.h"
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	/*copy contents of src[] to dest[]*/
+	for (n = 0; n != (void *)0; n++)
+	{
+		dest[n] = src[n];
+	}
+	return (dest);
+}
