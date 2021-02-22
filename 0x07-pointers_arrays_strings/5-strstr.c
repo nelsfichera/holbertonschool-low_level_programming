7e76acd357f0231ae976de8ad4ce6c2f2e32913c
Nels <nelsfichera@gmail.com>
2021-02-22 14:05:13 -0500

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#define NULL 0
 /**
 * _strstr - locates a substring
 * @haystack: the string
@@ -7,3 +8,37 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
+	char *a; /*for iteration*/
+	char *b; /*for iteration*/
+
+	b = needle;
+
+	if (*b == 0)
+	{
+		return ((char *) haystack);
+	}
+
+	for ( ; *haystack != 0; haystack++)
+	{
+		if (*haystack != *b)
+		{
+			continue;
+		}
+
+		a = haystack;
+
+		while (1)
+		{
+			if (*b == 0)
+			{
+				return ((char *) haystack);
+			}
+			if (*a++ != *b++)
+			{
+				break;
+			}
+		}
+		return (needle);
+	}
+	return (NULL);
+}
