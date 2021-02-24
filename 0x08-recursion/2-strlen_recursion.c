5ab711e6b59fbc2e3c995f94c3cde10d4eeeb272
Nels <nelsfichera@gmail.com>
2021-02-24 10:43:27 -0500

    Recursive strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
 /**
 * _strlen_recursion - return the length of a string
-* @s: pointer in the string 
+* @s: pointer in the string
 * Return: int
 */
 int _strlen_recursion(char *s)
 {
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
