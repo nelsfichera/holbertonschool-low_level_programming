284364e8e963a7d3ab37a6c9388db908b88fc4d7
Nels <nelsfichera@gmail.com>
2021-02-24 10:49:49 -0500

    Four, unknown segmentation fault, wait for checker

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,14 @@
 #include "holberton.h"
 /**
-* pow_recursion - return the value of x to the power of y
+* _pow_recursion - return the value of x to the power of y
 * @x: the integer
 * @y: the exponent
 * Return: int
 */
 int _pow_recursion(int x, int y)
 {
+	if (x >= 0)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (-1);
 }
