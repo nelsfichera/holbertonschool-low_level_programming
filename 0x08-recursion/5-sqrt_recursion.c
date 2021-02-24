124b91764ff0c8b7d372614d869da2009c7cc76f
Nels <nelsfichera@gmail.com>
2021-02-24 12:47:12 -0500

    Helper function then recurse

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,28 @@
 #include "holberton.h"
 /**
+* _sqrthelp - calculate the natural sqrt
+* @x: the number
+* @n: iteration
+* Return: result
+*/
+int _sqrthelp(int x, int n)
+{
+	if (n * n == x)
+	{
+		return (n);
+	}
+	if (n * n > x)
+	{
+		return (-1);
+	}
+	return (_sqrthelp(x, n + 1));
+}
+/**
 * _sqrt_recursion - return the natural square root
 * @n: the number
 * Return: int
 */
 int _sqrt_recursion(int n)
 {
-	if (n > 0)
-		return (n / _sqrt_recursion(n));
-	else
-		return (-1);
+	return (_sqrthelp(n, 1));
 }
