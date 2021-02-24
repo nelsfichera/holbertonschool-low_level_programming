043ff6dbdad76ecef25c3877784f702d79298161
Nels <nelsfichera@gmail.com>
2021-02-24 09:58:44 -0500

    betty compliant

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
 * factorial - reutn the factorial of a given number
-* @n: the number 
+* @n: the number
 * Return: int
 */
-int factorial (int n)
+int factorial(int n)
 {
 	if (n < 0)
 	{
@@ -14,5 +14,5 @@ int factorial (int n)
 	{
 		return (1);
 	}
-	return (n * factorial (n - 1));
+	return (n * factorial(n - 1));
 }
