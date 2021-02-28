7026d96bcaec1956d4e4cbeda4f420dc1af2746d
Nels <nelsfichera@gmail.com>
2021-02-28 14:44:41 -0500

    Asterisk placement

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -16,4 +16,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char _*strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
