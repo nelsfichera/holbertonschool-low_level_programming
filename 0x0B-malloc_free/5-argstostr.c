0251b15713a8f44533fe1b4427cce85916480292
Nels <nelsfichera@gmail.com>
2021-03-02 17:14:17 -0500

    Fix compile errors

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -10,33 +10,28 @@
 char *argstostr(int ac, char **av)
 {
 	int x, n = 0; /*for iteration*/
-	int position; /*position of pointer*/
-	int length; /*length of strings*/
+	int position = 0; /*position of pointer*/
+	int length = 0; /*length of strings*/
 	char *args; /*strings of arguments*/
-	
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	
-	for (x = 0; x < ac; x++)
-	{
-		for (n = 0; av[x][n] != '\0'; n++)
-		{
-			length++;
-		}
-	}
 
+	for (x = 0; x < ac; x++)
+		for (n = 0; av[x][n] != '\0'; n++)
+			length++;
 	args = malloc((length + ac + 1) * sizeof(char));
-	
-	if args == (NULL)
+
+	if (args == NULL)
 	{
 		free(args);
 		return (NULL);
 	}
 	for (x = 0; x < ac; x++)
 	{
-		for (n = 0; av [x][n] != '\0'; n++)
+		for (n = 0; av[x][n] != '\0'; n++)
 		{
-			args = av[x][n];
+			args = &av[x][n];
 			position++;
 		}
 		args[position] = '\n';
