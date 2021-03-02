12d4ce07000c66f658a091783e8b51e80a777ad5
Nels <nelsfichera@gmail.com>
2021-03-02 14:18:21 -0500

    Use blank for loop

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,8 +15,9 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	while (str[n] != '\0')
-		n++;
+	for (x = 0; str[x]; x++)
+	{}
+	n = x + 1;
 
 	copy = malloc(sizeof(char) * n + 1);
 
