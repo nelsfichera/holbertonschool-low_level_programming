780aa0cec2e3a9dc03b761af64b2b7a3499315a5
Nels <nelsfichera@gmail.com>
2021-03-08 11:18:54 -0500

    Let the dog out

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - lets the dog out
+* @d: pointer to dog
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
