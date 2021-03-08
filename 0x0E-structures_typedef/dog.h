a73990ee33beedc84413535196210d7b22d6ffe8
Nels <nelsfichera@gmail.com>
2021-03-08 11:17:52 -0500

    Syntax fixes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+* struct dog - info re: dog
+* @name: name of dog
+* @age: age of dog
+* @owner: who takes care of the dog
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
