dbbaf562947b7c9bc72839b6c8a48635336d348c
Nels <nelsfichera@gmail.com>
2021-03-10 10:11:51 -0500

    If for if

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+* int_index - searches for an integer
+* @array: target array
+* @size: size of the array
+* @cmp: comparison function
+* Return: on success, index of first matching element.
+*	if size <0 or no matches, returns -1.
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int res = 0;
+
+	if (array && size && cmp)
+		for (res = 0; res < size; res++)
+			if (cmp(array[res]))
+					return (res);
+
+	return (-1);
+}
