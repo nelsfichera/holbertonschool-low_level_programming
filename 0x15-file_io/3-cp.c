b919d67f493d31e85017a7f2a349fef528cf6c8f
Nels <nelsfichera@gmail.com>
2021-03-31 09:27:43 -0400

    Tile to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from tile_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (argv[1] == NULL)
