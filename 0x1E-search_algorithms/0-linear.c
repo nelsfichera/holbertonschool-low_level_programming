4dc0251ff3a75f24e1bc7fbf46b159b82d9992a7
Nels <nelsfichera@gmail.com>
2021-09-02 08:31:35 -0400

    Fix semicolon

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t index = 0;
 
 	if (array)
-		for (; index <= size; array++; index++)
+		for (; index <= size; array++, index++)
 		{
 			printf("Value checked array[%lu] = [%d]\n", index, *array);
 			if (value == *array)
